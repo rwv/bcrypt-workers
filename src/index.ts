4e773f917d53e45f0d3cb3893184a55338b64ed9
Seedgou <i@zczc.cz>
2024-05-12 10:33:21 +0800

    feat: remove task related endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,25 @@
 import { OpenAPIRouter } from "@cloudflare/itty-router-openapi";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
 import { Hash } from "endpoints/hash";
 import { Verify } from "endpoints/verify";
 
 export const router = OpenAPIRouter({
-	docs_url: "/",
+  docs_url: "/",
 });
 
-router.get("/api/tasks/", TaskList);
-router.post("/api/tasks/", TaskCreate);
-router.get("/api/tasks/:taskSlug/", TaskFetch);
-router.delete("/api/tasks/:taskSlug/", TaskDelete);
 router.post("/hash", Hash);
 router.post("/verify", Verify);
 
 // 404 for everything else
 router.all("*", () =>
-	Response.json(
-		{
-			success: false,
-			error: "Route not found",
-		},
-		{ status: 404 }
-	)
+  Response.json(
+    {
+      success: false,
+      error: "Route not found",
+    },
+    { status: 404 }
+  )
 );
 
 export default {
-	fetch: router.handle,
+  fetch: router.handle,
 };
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,7 @@
-import { DateTime, Str, Num } from "@cloudflare/itty-router-openapi";
+import { Str, Num } from "@cloudflare/itty-router-openapi";
 
-export const Task = {
-	name: new Str({ example: "lorem" }),
-	slug: String,
-	description: new Str({ required: false }),
-	completed: Boolean,
-	due_date: new DateTime(),
-};
-
-
-export const Password = new Str({ required: true, example: "password"})
-export const Rounds = new Num({ default: 10, required: false, example: 10})
-export const Hash = new Str({ example: "$2a$10$0AeRIW6lLG4Nz5uCLItonuasTNdScKoLzBX9hIzIII371CxBcwYrO"})
+export const Password = new Str({ required: true, example: "password" });
+export const Rounds = new Num({ default: 10, required: false, example: 10 });
+export const Hash = new Str({
+  example: "$2a$10$0AeRIW6lLG4Nz5uCLItonuasTNdScKoLzBX9hIzIII371CxBcwYrO",
+});
